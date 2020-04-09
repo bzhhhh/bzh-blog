92e9cda70ae6070ef5d576373b9eb0e6c3bc040f
baobao <923065971@qq.com>
2020-04-09 16:28:19 +0800

    0409 add sass

--- components/Layout.js
+++ components/Layout.js
@@ -7,12 +7,14 @@ export default ({ title, children }) => (
     <Head>
       <meta name='viewport' content='width=device-width, initial-scale=1' />
       <meta charSet='utf-8' />
+      <link rel="icon" href="/favicon.ico" />
       <title>bzh</title>
     </Head>
     <style jsx global>{`
       * {
         margin: 0;
         padding: 0;
+        box-sizing: border-box;
       }
       body {
         font-family: Helvetica, 'Hiragino Sans GB', 'Microsoft Yahei', '微软雅黑', Arial, sans-serif;
--- pages/user.js
+++ pages/user.js
@@ -1,5 +1,18 @@
+import Layout from '../components/Layout'
 const UserList = () => (
-  <h3>user1:bzh</h3>
+  <Layout>
+    <h3>user1:bzh</h3>
+    <style jsx>
+      {
+        `
+      h3 {
+        color: #94e0fb;
+      }
+      `
+      }
+    </style>
+  </Layout>
+
 )
 
 export default UserList
